5ab0559fa77820451cc0c8a0caaa301c72896adc
KIRTI G <kirtig@kirtis-MacBook-Pro.local>
2026-02-02 18:06:19 +0530

    feat: Add Next.js Image component to ProductCard and configure remote image domains in next.config.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'indeloom.com' },
+      { protocol: 'https', hostname: 'www.mysoresareeudyog.com' },
+      { protocol: 'https', hostname: 'indiahandmade.com' },
+      { protocol: 'https', hostname: 'sutraclothing.in' },
+      { protocol: 'https', hostname: 'www.jaypore.com' },
+      { protocol: 'https', hostname: 'itokri.com' },
+      { protocol: 'https', hostname: 'trovecraftindia.com' },
+      { protocol: 'https', hostname: 'zishta.com' },
+      { protocol: 'https', hostname: 'thebombaystore.com' },
+      { protocol: 'https', hostname: 'eyaas.in' },
+      { protocol: 'https', hostname: 'shop.gaatha.com' },
+      { protocol: 'https', hostname: 'www.kamaayurveda.in' },
+      { protocol: 'https', hostname: 'kapiva.in' },
+      { protocol: 'https', hostname: 'happyherbalcare.in' },
+      { protocol: 'https', hostname: 'nathabit.in' },
+      { protocol: 'https', hostname: 'forestessentialsindia.com' },
+      { protocol: 'https', hostname: 'amrutam.co.in' },
+      { protocol: 'https', hostname: 'bambooindia.com' },
+      { protocol: 'https', hostname: 'refreshyourlife.in' },
+      { protocol: 'https', hostname: 'alfagiftbox.in' },
+      { protocol: 'https', hostname: 'offistore.in' },
+      { protocol: 'https', hostname: 'brownliving.in' },
+      { protocol: 'https', hostname: 'pack-mate.in' },
+      { protocol: 'https', hostname: 'm.indiamart.com' },
+      { protocol: 'https', hostname: 'placehold.co' },
+      { protocol: 'https', hostname: 'image.pollinations.ai' },
+      { protocol: 'https', hostname: 'loremflickr.com' },
+      { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: 'fastly.picsum.photos' }
+    ],
+  },
 };
 
 export default nextConfig;
