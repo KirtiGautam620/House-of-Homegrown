30fd2a9fb26a6890d77832aaebfc64973f711fe1
KIRTI G <kirtig@kirtis-MacBook-Pro.local>
2026-02-02 12:29:25 +0530

    fixing err

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        unique: true,
-        required: true
+    id:{
+        type:Number,
+        unique:true,
+        required:true
     },
-    name: {
-        type: String,
-        required: true
+    name:{
+        type:String,
+        required:true
     },
-    description: {
-        type: String
+    description:{
+        type:String
     },
-    price: {
-        type: Number,
-        required: true
+    price:{
+        type:Number,
+        required:true
     },
-    image: {
-        type: String
+    image:{
+        type:String
     },
-    categoryId: {
-        type: Number,
-        required: true
+    categoryId:{
+        type:Number,
+        required:true
     },
-    createdAt: {
-        type: Date,
-        default: Date.now
+    featured:{
+        type:Boolean,
+        default:false
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
     }
-
 });
 module.exports = mongoose.model("Product", productSchema);
