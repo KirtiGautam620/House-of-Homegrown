eabc52cb056e10cb5440acc7cee935832952490e
Kirti Gautam <gautamkirti620@gmail.com>
2026-03-16 12:00:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const Product = require("./models/Product");
 const productRoutes = require("./routes/productRoutes")
 const categoryRoutes = require("./routes/categoryRoutes")
+const authRoutes = require("./routes/authRoutes")
 connectDB();
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(cors())
 app.use(express.json());
 app.use("/api/products", productRoutes)
 app.use("/api/categories", categoryRoutes)
+app.use("/api/auth", authRoutes)
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
