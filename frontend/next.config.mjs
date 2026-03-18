74fba777d84f6bed22f5147f6cbe9501ec612fc3
Kirti Gautam <gautamkirti620@gmail.com>
2026-03-18 12:00:00 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,9 +6,9 @@ const nextConfig = {
       { protocol: 'https', hostname: 'loremflickr.com' },
       { protocol: 'https', hostname: 'fastly.picsum.photos' },
       { protocol: 'https', hostname: 'encrypted-tbn3.gstatic.com' },
-      { protocol: 'https', hostname: 'cdn.sareeka.com' },
+      { protocol: 'https', hostname: 'cdn.sareeka.com' }, 
       { protocol: 'https', hostname: 'vipnari.com' },
-      { protocol: 'https', hostname: 'pashtush.in' },
+      { protocol: 'https', hostname: 'pashtush.in' }, 
       { protocol: 'https', hostname: 'jhakhas.com' },
       { protocol: 'https', hostname: 'wholetex.sgp1.cdn.digitaloceanspaces.com' }
     ],
